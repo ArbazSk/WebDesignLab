696f78e5f35f46de3747a7cc6878144048b393f8
moingeek <gadmoin@gmail.com>
2018-08-17 11:19:59 +0530

    done

--- js/app.js
+++ js/app.js
@@ -18,24 +18,27 @@ $(function(){
 		}
 
 	});
-	$('#address').on('blur',function(e){
+
+	$('#address').on('blur',function(){
+		console.log(this.value.length);
+
 		if(this.value.length<50 && this.value.length>150){
 			alert("Invalid address. Length must be between 50 to 150 chararacter");
-
+			this.value="";
+			$(this).focus();
 		}
-		this.value="";
-		$(this).focus();
 
 	});
 
-	$('#semail').on('blur',function(e){
-		if(/((\w*.?_?-?\d*\w+)@(\w*-?.?\d*\w+).(\w*-?))/.test(e.key)){
-			alert("Invalid Email.");
-		}
-		this.value="";
-		$(this).focus();
+	// $('#semail').on('blur',function(){
+	// 	if(/((\w*.?_?-?\d*\w+)@(\w*-?.?\d*\w+).(\w*-?))/.test(this.value)){
+	// 		alert("Invalid Email.");
+	// 		this.value="";
+	// 		$(this).focus();
+	// 	}
+		
 
 
-	});
+	// });
 
 });
